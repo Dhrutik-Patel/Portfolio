d5577e0d54034529fb212111e6589cc84bccb900
Dhrutik-Patel <dhrutikpatel2017@gmail.com>
2024-03-17 21:07:24 +0530

    [IMP] portfolio: implemented home page component

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a
+                href="https://github.com/Dhrutik-Patel"
+                className="home__social-icon"
+                target="_blank"
+            >
+                <i className="uil uil-github-alt"></i>
+            </a>
+            <a
+                href="https://www.linkedin.com/in/dhrutik-patel/"
+                className="home__social-icon"
+                target="_blank"
+            >
+                <i className="uil uil-linkedin-alt"></i>
+            </a>
+            <a
+                href="https://www.instagram.com/dhrutik._.patel"
+                className="home__social-icon"
+                target="_blank"
+            >
+                <i className="uil uil-instagram"></i>
+            </a>
+        </div>
+    );
+};
+
+export default Social;
